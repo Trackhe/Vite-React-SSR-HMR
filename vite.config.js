02719804adcb1f9d7d7d170410d8b26e4fc403b6
Michael Nehring <michael@trackhe.de>
2022-06-05 13:49:49 +0200

    Clean a bit up

--- vite.config.js
+++ vite.config.js
@@ -3,51 +3,33 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import fs from 'fs'
 
-function configuretheServer(server, dorp) {
-
-  server.middlewares.use(async (req, res, next) => {
-    if (req.url !== "/") {
-      return next()
-    }
-
-    console.log(dorp) //Check the running server mode
-    const { renderInNode } = await server.ssrLoadModule(path.resolve(__dirname, "./server/src/index"))
-
-    const indexHtml = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8")
-
-    const url = new URL("http://localhost:3000/" + req.url)
-    const template = await server.transformIndexHtml(url.toString(), indexHtml)
-
-    const head = template.match(/<head>(.+?)<\/head>/s)[1]
-
-    return renderInNode( res, head )
-  });
-}
-
 function ssrPlugin() {
-  /**
-   * @type {import('vite').Plugin}
-   */
   return {
     name: "ssrPlugin",
-    configureServer(server){configuretheServer(server, "dev")},
-    configurePreviewServer(server){configuretheServer(server, "preview")}
-  };
+    configureServer(server){
+      server.middlewares.use(async (req, res, next) => {
+        if (req.url !== "/") {
+          return next()
+        }
+
+        const { renderInNode } = await server.ssrLoadModule(path.resolve(__dirname, "./server/src/index"))
+        console.log("before error")
+        const indexHtml = fs.readFileSync(path.resolve(__dirname, "./server/src/index.html"), "utf-8")
+    
+        const url = new URL("http://localhost:3000/" + req.url)
+        const template = await server.transformIndexHtml(url.toString(), indexHtml)
+    
+        const head = template.match(/<head>(.+?)<\/head>/s)[1]
+    
+        return renderInNode( res, head )
+      })
+    }
+  }
 }
 
-export default defineConfig(({ command, mode }) => {
-  console.log(command + " : " + mode)
-  if (command === "serve" && mode === "development") {
-    return {
-      plugins: [react(), ssrPlugin()]
-    }
-  } else {
-    // command === 'build'
-    return {
-      plugins: [react(), ssrPlugin()],
-      build: {
-        minify: false
-      }
-    }
+export default defineConfig(() => {
+  return {
+    server: { port: 3000 },
+    plugins: [react(), ssrPlugin()]
   }
 })
