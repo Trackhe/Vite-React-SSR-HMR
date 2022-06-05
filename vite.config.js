d8e0ac2cab705970d29048e6a862823e610c8f0c
Trackhe <michael@trackhe.de>
2022-06-05 09:03:53 +0000

    Get it back to work.

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import fs from 'fs';
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import fs from 'fs'
 
 function configuretheServer(server, dorp) {
-  console.log(dorp)
+
   server.middlewares.use(async (req, res, next) => {
     if (req.url !== "/") {
       return next()
     }
 
-    if(dorp === "dev") {
-      const { renderInNode } = await server.ssrLoadModule(path.resolve(__dirname, "./server/src/index"))
-    } else {
-      const { renderInNode } = require("./dist/server/index.js")
-    }
+    console.log(dorp) //Check the running server mode
+    const { renderInNode } = await server.ssrLoadModule(path.resolve(__dirname, "./server/src/index"))
+
     const indexHtml = fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8")
 
     const url = new URL("http://localhost:3000/" + req.url)
